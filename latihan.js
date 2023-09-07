9ede9643a1ea917054b45ec9e06f2d2dd972acb0
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2023-09-07 15:21:23 +0800

    Add files via upload

---
+++ latihan.js
@@ -0,0 +1,5 @@
+var name1 = prompt("Gilr name : ");
+var name2 = prompt("Boy name: ");
+var n = Math.random();
+n = Math.floor(n * 100)+1;
+prompt(name1 + "&" + name2 + ", Kecocokan "+ n + "%");ReferenceError
